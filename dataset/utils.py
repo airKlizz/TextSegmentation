ea627947788a873e964000a6e19f66bb017d4513
remi calizzano <remi.calizzano@gmail.com>
2020-04-09 18:46:28 +0200

    Change data format

--- dataset/utils.py
+++ dataset/utils.py
@@ -8,20 +8,24 @@ import json
 from tqdm import tqdm
 from model.segmenter import Segmenter
 
-def create_tf_dataset(train_path='wikinews/data.jsonl', max_sentences=32, test_size=0.2, batch_size=16, shuffle=10000, random_state=2020):
+def create_tf_dataset(train_path='wikinews/train.txt', max_sentences=32, test_size=0.2, batch_size=16, shuffle=10000, random_state=2020):
     X = []
     y = []
     mask = []
     with open(train_path, 'r') as f:
-        for line in tqdm(f, desc="Reading train file"):
-            json_line = json.loads(line)
-            passages = json_line['passages']
+        data = f.read()
+        for article in tqdm(data.split('\n\n'), desc="Reading train file"):
             X_ = []
             y_ = []
-            for passage in passages:
-                sentences = sent_tokenize(passage)
-                X_ += sentences
-                y_ += [[0, 1]]+[[1, 0]]*(len(sentences)-1)
+            sentences = article.split('\n')
+            for sentence in sentences[1:]:
+                elems = sentence.split('\t')
+                assert len(elems) == 4
+                X_.append(elems[-2])
+                if int(elems[-1]) == 1:
+                    y_.append([0, 1])
+                else:
+                    y_.append([1, 0])
             X.append(X_)
             y.append(y_)
             mask.append([1]*len(y_))
@@ -29,6 +33,12 @@ def create_tf_dataset(train_path='wikinews/data.jsonl', max_sentences=32, test_s
     X = Segmenter.prepare_inputs(X, max_sentences, return_tf=False)
     y = Segmenter.prepare_inputs(y, max_sentences, return_tf=False, pad=[1, 0])
     mask = Segmenter.prepare_inputs(mask, max_sentences, return_tf=False, pad=0)
+
+    assert len(X) == len(y) and len(X) == len(mask)
+    assert len(X[0]) == len(X[1])
+    assert type(X[0][0]) == str
+    assert type(y[0][0]) == list
+    assert type(mask[0][0]) == int
 
     train_X, validation_X, train_y, validation_y, train_mask, validation_mask = train_test_split(X, y, mask, random_state=random_state, test_size=test_size)
     train_dataset = tf.data.Dataset.from_tensor_slices((train_X, train_y, train_mask)).shuffle(shuffle).batch(batch_size)
